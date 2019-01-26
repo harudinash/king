144349d7faa366c0e81a2b2e7dfeb9a1db80fa42
harudinash <46379892+harudinash@users.noreply.github.com>
2019-01-26 08:15:00 +0530

    Create BEG_1_7.C

---
+++ BEG_1_7.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{   
+    int n;
+    printf("INPUT\n");
+    scanf("%d",&n);
+    printf("\nOUTPUT\n");
+    while(n)
+    {
+    printf("Hello\n");
+    n--;
+    }
+    return 0;
+}
